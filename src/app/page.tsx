83fdef3be98d96918db2b5ee50ffa92a5f0771eb
snacksncode <snacksncode@gmail.com>
2023-04-26 21:24:47 +0200

    feat: style experience section, mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/Hero";
 import styles from "./page.module.css";
 import { Navbar } from "@/components/Navbar";
+import { Experience } from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <main className={styles.main}>
         <Hero />
+        <Experience />
       </main>
     </>
   );
