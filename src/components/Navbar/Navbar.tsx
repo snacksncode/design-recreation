d0d68c186b221bd58e90bfedd9dffabcab4261ae
snacksncode <snacksncode@gmail.com>
2023-10-11 16:17:39 +0200

    feat: update Navbar styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,20 +6,20 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <nav className={styles.wrapper}>
-      <Link href="/" className={styles.logo}>
-        <Logo />
+    <nav className={styles["wrapper"]}>
+      <Link href="/" className={styles["logo"]}>
+        <Logo className={styles["logo__svg"]} />
       </Link>
-      <div className={styles.nav}>
-        <button className={styles.status}>Flight Status</button>
-        <button className={styles["check-in"]}>Check In</button>
-        <button className={styles.avatar}>
+      <div className={styles["nav"]}>
+        <button className={styles["nav__status"]}>Flight Status</button>
+        <button className={styles["nav__check-in"]}>Check In</button>
+        <button className={styles["nav__avatar"]}>
           <Image src={avatar} alt="Avatar" />
         </button>
-        <button className={styles.hamburger}>
-          <div className={styles.line} />
-          <div className={styles.line} />
-          <div className={styles.line} />
+        <button className={styles["hamburger"]}>
+          <div className={styles["hamburger__line"]} />
+          <div className={styles["hamburger__line"]} />
+          <div className={styles["hamburger__line"]} />
         </button>
       </div>
     </nav>
